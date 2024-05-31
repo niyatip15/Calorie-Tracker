0a5a629a26817b747d2ba93c9875fdaedf2a1f1e
niyatip15 <niyupathak1503@gmail.com>
2024-05-31 18:19:43 +0530

    login,register,logout user wise data

--- mysite/calorie_tracker/urls.py
+++ mysite/calorie_tracker/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . views import *
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('',views.index, name='index'),
+    path('login/',CustomLoginView.as_view(), name='login'),
+    path('register/',Registeration.as_view(), name='register'),
+     path('logout/',LogoutView.as_view(next_page='login'),name='logout'),
     path('delete/<int:id>/',views.delete_consumed_food, name='delete')
     
 ]
--- mysite/calorie_tracker/views.py
+++ mysite/calorie_tracker/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render, redirect
 from calorie_tracker.models import Food,ConsumeFood
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from django.contrib.auth import login
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+    
+    
+class Registeration(FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('index')
+    redirect_authenticated_user = True
+
+    def form_valid(self,form):
+        user = form.save()
+        if user is not None:
+            login(self.request,user)
+        return super(Registeration,self).form_valid(form)
+    
+    def get(self,*args,**kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('index')
+        return super(Registeration,self).get(*args, **kwargs)
+    
 
 def index(request):
     context = {}
